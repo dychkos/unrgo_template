b611c29b65c85ae203938e1225663c6a333e1199
Sergey Dychko <dychkosergey@gmail.com>
2022-02-08 10:12:58 +0200

    added product page template

--- app/js/store.js
+++ app/js/store.js
@@ -19,10 +19,27 @@ new Swiper(".popular-swiper", {
 * Nodes
 * */
 let navigation = document.querySelector('.navigation__active');
+let sort = document.querySelector('.sort');
 
 /*
 * Event listeners
 * */
+sort.addEventListener('click',()=>{
+    let sortBody = sort.querySelector('.sort__body');
+
+    let hider = new Hider(".sort", ()=>{
+        $(".sort__body").removeClass("active");
+    });
+
+    if(sortBody.classList.contains("active")){
+        sortBody.classList.remove("active");
+        document.removeEventListener('click',hider.hide);
+    }else{
+        sortBody.classList.add("active");
+        document.addEventListener('click',hider.hide);
+    }
+
+})
 navigation.addEventListener("click",()=>{
     let navBody = document.querySelector(".navigation__body");
 
